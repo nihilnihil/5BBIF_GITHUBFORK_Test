08e3d54f7248b4bbf5fd7d7538f4982c8b56f43c
wasgehtda <hojati.a93@htlwienwest.at>
2022-05-25 19:32:25 +0200

    fgsdh

---
+++ src/hojati.java
@@ -0,0 +1,18 @@
+public class hojati {
+    String str;
+
+    public hojati(){ str = new String("Ein Witz");}
+
+
+    public String toString() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    public String getStr() {
+        return str;
+    }
+}
