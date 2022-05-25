a66c65568321b8bb6088c77e20e1322aeb036984
Rainer Fritz <rain3rfritz@gmail.com>
2022-05-25 18:54:45 +0200

    Inital Class for FRITZ

---
+++ src/FRITZ_TestClass.java
@@ -0,0 +1,17 @@
+public class FRITZ_TestClass {
+
+    String myString;
+
+    public FRITZ_TestClass() {
+        this.myString = "Allen steht das Wasser bis zum Hals nur nicht...";
+    }
+
+
+    public String getMyString() {
+        return myString;
+    }
+
+    public void setMyString(String myString) {
+        this.myString = myString;
+    }
+}
