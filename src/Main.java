af835e7434da0b2dfae9094b546e29cafe9274f7
Tazekar <93832771+Tazekar@users.noreply.github.com>
2022-05-25 18:57:12 +0200

    newly changed by Ali

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         BEDA_TestClass beda_testClass = new BEDA_TestClass();
+        Joshani_TestClass joshani_testClass = new Joshani_TestClass();
         System.out.println(beda_testClass.toString());
+        System.out.println(joshani_testClass.toString());
     }
 }
