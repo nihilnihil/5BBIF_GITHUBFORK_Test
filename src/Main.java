bb715a389cede10eaf960c01b4326bf8ec810f20
Anubis2500 <104906377+Anubis2500@users.noreply.github.com>
2022-05-25 18:53:56 +0200

    new class and lines

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
         System.out.println("Hello world!");
         BEDA_TestClass beda_testClass = new BEDA_TestClass();
         System.out.println(beda_testClass.toString());
+
+        Kwech_TestClass kw = new Kwech_TestClass();
+        System.out.println(kw.toString());
     }
 }
